166558aedccf621900cfbd16c1f738a40245378f
Polezhaiev Andrey <polezhaievandrey28@gmail.com>
2022-09-03 16:48:39 +0300

    Lottery!!!

--- src/main/java/com/hillel/polezhaiev/lessons/homework6/Main.java
+++ src/main/java/com/hillel/polezhaiev/lessons/homework6/Main.java
@@ -1,2 +1,32 @@
-package com.hillel.polezhaiev.lessons.homework6;public class Main {
+package com.hillel.polezhaiev.lessons.homework6;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        int lottery = (int) (Math.random() * 11);
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a number from 0 to 10: ");
+
+        while (true) {
+            if (scanner.hasNextInt()) {
+            int number = scanner.nextInt();
+
+                if (number == lottery) {
+                    System.out.println("You entered a true value!");
+                    break;
+                } else {
+                    System.out.println();
+                    System.out.println("Try again!");
+                    System.out.print("Enter a value: ");
+                }
+            } else {
+                System.out.println();
+                System.out.println("FALSE data!!!");
+                System.out.print("Enter again: ");
+                scanner.nextLine();
+            }
+        }
+    }
 }
