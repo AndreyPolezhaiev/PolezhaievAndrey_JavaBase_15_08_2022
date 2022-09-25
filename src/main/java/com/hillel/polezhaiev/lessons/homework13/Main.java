18929ef117682e288d2438bcbca8464f188a98c7
Polezhaiev Andrey <polezhaievandrey28@gmail.com>
2022-09-25 19:10:32 +0300

    Getters & Setters

--- src/main/java/com/hillel/polezhaiev/lessons/homework13/Main.java
+++ src/main/java/com/hillel/polezhaiev/lessons/homework13/Main.java
@@ -1,2 +1,36 @@
-package com.hillel.polezhaiev.lessons.homework13;public class Main {
+package com.hillel.polezhaiev.lessons.homework13;
+
+public class Main {
+    public static void main(String[] args) {
+        User user1 = new User("Andrey", "Polezhaiev", 8, 4, 2004,
+                "polez@gmail.com", "09876543211", 66, "120/80", 8760);
+
+        User user2 = new User("Artem", "Orlov", 5, 8, 2003,
+                "artem@gmail.com", "0983456735", 75, "140/100", 10000);
+
+        User user3 = new User("Nikita", "Yavnikov", 21, 7, 2005,
+                "nikita@gmail.com", "0662345678", 63, "130/90", 5690);
+
+
+        user1.printAccountInfo();
+        System.out.println();
+        user2.printAccountInfo();
+        System.out.println();
+        user3.printAccountInfo();
+        System.out.println();
+
+        System.out.println("Changed");
+
+        user2.weight = 65;
+        user2.getEmail("art2684@gmail.com");
+        user3.getPhone("0977887654");
+        user3.getYear(2002);
+        user3.getEmail("Nik37667@gmail.com");
+
+        user2.printAccountInfo();
+        System.out.println();
+        user3.printAccountInfo();
+        System.out.println();
+
+    }
 }
--- src/main/java/com/hillel/polezhaiev/lessons/homework13/User.java
+++ src/main/java/com/hillel/polezhaiev/lessons/homework13/User.java
@@ -1,2 +1,77 @@
-package com.hillel.polezhaiev.lessons.homework13;public class User {
+package com.hillel.polezhaiev.lessons.homework13;
+
+public class User {
+    private String name;
+    private int day;
+    private int month;
+    private int year;
+    private String email;
+    private String phone;
+    private int age;
+
+    public String getName(String name) {
+        this.name = name;
+        return this.name;
+    }
+
+    public int getDay(int day) {
+        this.day = day;
+        return this.day;
+    }
+
+    public int getMonth(int month) {
+        this.month = month;
+        return this.month;
+    }
+
+    public int getYear(int year) {
+        this.year = year;
+        return this.year;
+    }
+
+    public String getEmail(String email) {
+        this.email = email;
+        return this.email;
+    }
+
+    public String getPhone(String phone) {
+        this.phone = phone;
+        return this.phone;
+    }
+
+    protected String surname;
+    protected int weight;
+    protected String press;
+    protected int steps;
+
+    public User(String name, String surname, int day, int month, int year, String email, String phone, int weight, String press, int steps) {
+        this.name = name;
+        this.day = day;
+        this.month = month;
+        this.year = year;
+        this.email = email;
+        this.phone = phone;
+        this.surname = surname;
+        this.weight = weight;
+        this.press = press;
+        this.steps = steps;
+    }
+
+    public int getAge() {
+        age = 2020 - this.year;
+        return age;
+    }
+
+    protected void printAccountInfo(){
+        System.out.println("Name: " + this.name);
+        System.out.println("Surname: " + this.surname);
+        System.out.println("Birthday: " + this.day + "." + this.month + "." + this.year);
+        System.out.println("Age: " + getAge());
+        System.out.println("Email: " + this.email);
+        System.out.println("Phone: " + this.phone);
+        System.out.println("Weight: " + this.weight);
+        System.out.println("Pressure: " + this.press);
+        System.out.println("Amount of steps: " + this.steps);
+    }
+
 }
