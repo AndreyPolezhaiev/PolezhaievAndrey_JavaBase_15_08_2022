cc537e502d217e5933fd23b12f42e6004e427d35
Polezhaiev Andrey <polezhaievandrey28@gmail.com>
2022-09-29 10:40:19 +0300

    Poliforizm was made better

--- src/main/java/com/hillel/polezhaiev/lessons/homework14/Main.java
+++ src/main/java/com/hillel/polezhaiev/lessons/homework14/Main.java
@@ -7,61 +7,64 @@ public class Main {
 //        MusicStyles classicSong = new ClassicMusic();
 //        MusicStyles popSong = new PopMusic();
 
-        MusicStyles[] rockGroup = {
-                new RockMusic("Andrey"),
-                new RockMusic("Kirill"),
-                new RockMusic("Denis")
-        };
-
-        MusicStyles[] popGroup = {
-                new PopMusic("Jenya"),
-                new PopMusic("Fedya"),
-                new PopMusic("Danya")
-        };
-
-        MusicStyles[] classicGroup = {
-                new ClassicMusic("Katya"),
-                new ClassicMusic("Masha"),
-                new ClassicMusic("Liza")
-        };
 
 
 
-
-        System.out.println();
-
-        for(MusicStyles kindOfMusic: rockGroup){
-                kindOfMusic.playMusic();
-        }
-        System.out.println();
-
-        for(MusicStyles kindOfMusic: popGroup){
-            kindOfMusic.playMusic();
-        }
-        System.out.println();
-
-        for(MusicStyles kindOfMusic: classicGroup){
-            kindOfMusic.playMusic();
-        }
-
-
-//        MusicStyles[][] HOR = {
-//                ////////RockMusicHor///////
-//                {new RockMusic("Andrey"), new RockMusic("Kirill"), new RockMusic("Denis")},
-//
-//                ////////PopMusicHor///////
-//                {new PopMusic("Jenya"), new PopMusic("Fedya"), new PopMusic("Danya")},
-//
-//                ////////ClassicMusicHor///////
-//                { new ClassicMusic("Katya"), new ClassicMusic("Masha"), new ClassicMusic("Liza")}
+//        MusicStyles[] rockGroup = {
+//                new RockMusic("Andrey"),
+//                new RockMusic("Kirill"),
+//                new RockMusic("Denis")
 //        };
 //
-//        for(MusicStyles[] hor: HOR){
-//            for(MusicStyles[] kindOfMusic: HOR){
+//        MusicStyles[] popGroup = {
+//                new PopMusic("Jenya"),
+//                new PopMusic("Fedya"),
+//                new PopMusic("Danya")
+//        };
+//
+//        MusicStyles[] classicGroup = {
+//                new ClassicMusic("Katya"),
+//                new ClassicMusic("Masha"),
+//                new ClassicMusic("Liza")
+//        };
+
+
+
+
+        System.out.println();
+
+//        for(MusicStyles kindOfMusic: rockGroup){
 //                kindOfMusic.playMusic();
-//            }
+//        }
+//        System.out.println();
+//
+//        for(MusicStyles kindOfMusic: popGroup){
+//            kindOfMusic.playMusic();
+//        }
+//        System.out.println();
+//
+//        for(MusicStyles kindOfMusic: classicGroup){
+//            kindOfMusic.playMusic();
 //        }
 
+
+        MusicStyles[][] HOR = {
+                ////////RockMusicHor///////
+                {new RockMusic("Andrey"), new RockMusic("Kirill"), new RockMusic("Denis")},
+
+                ////////PopMusicHor///////
+                {new PopMusic("Jenya"), new PopMusic("Fedya"), new PopMusic("Danya")},
+
+                ////////ClassicMusicHor///////
+                { new ClassicMusic("Katya"), new ClassicMusic("Masha"), new ClassicMusic("Liza")}
+        };
+
+        for(MusicStyles[] hor: HOR){
+            for (MusicStyles musicStyles : hor) {
+                musicStyles.playMusic();
+            }
+            System.out.println();
+        }
 
     }
 }
