08247dc01774686b2807107adaf59e4b18de4332
Polezhaiev Andrey <polezhaievandrey28@gmail.com>
2022-10-09 10:03:18 +0300

    Try to new proj

--- src/main/java/com/hillel/polezhaiev/lessons/lhomework1/Main.java
+++ src/main/java/com/hillel/polezhaiev/lessons/lhomework1/Main.java
@@ -1,2 +1,7 @@
-package com.hillel.polezhaiev.lessons.lhomework1;public class Main {
+package com.hillel.polezhaiev.lessons.lhomework1;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("hello new project");
+    }
 }
